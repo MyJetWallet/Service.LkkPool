5f26a3d91d7ee1abaa529b229c5bb4b723939f34
alexey <alexey.n@smplt.net>
2021-10-07 10:17:26 +0300

    add tool

--- test/TestApp/Program.cs
+++ test/TestApp/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Lykke.HftApi.ApiContract;
 using ProtoBuf.Grpc.Client;
 using Service.LkkPool.Client;
 using Service.LkkPool.Grpc.Models;
+using Service.LkkPool.Services;
 
 namespace TestApp
 {
@@ -10,17 +14,38 @@ namespace TestApp
     {
         static async Task Main(string[] args)
         {
-            GrpcClientFactory.AllowUnencryptedHttp2 = true;
+            var apikey = "";
+            
+            var client = new TradingApiClient("https://hft-apiv2-grpc.lykke.com", apikey);
 
-            Console.Write("Press enter to start");
-            Console.ReadLine();
+            var ts = DateTime.Parse("2021-10-05T19:43:44Z");
+            ts = DateTime.SpecifyKind(ts, DateTimeKind.Utc);
+            
+            Console.WriteLine(ts);
+            
+            
+            var data = await client.PrivateApi.GetTradesAsync(new TradesRequest()
+            {
+                AssetPairId = "ETHUSD",
+                From = Timestamp.FromDateTime(ts),
+                Take = 1000
+            });
 
-
-            var factory = new LkkPoolClientFactory("http://localhost:5001");
-            var client = factory.GetHelloService();
-
-            var resp = await  client.SayHelloAsync(new HelloRequest(){Name = "Alex"});
-            Console.WriteLine(resp?.Message);
+            var eth = 0m;
+            var usd = 0m;
+            
+            foreach (var trade in data.Payload.OrderBy(e => e.Timestamp))
+            {
+                eth += (trade.Side == Side.Buy ? 1 : -1) * decimal.Parse(trade.BaseVolume);
+                usd += (trade.Side == Side.Buy ? -1 : 1) * decimal.Parse(trade.QuoteVolume);
+                Console.WriteLine($"{trade.Timestamp:HH:mm:ss} {trade.Side} {trade.BaseVolume} {trade.Price} [{trade.QuoteVolume}]");
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("---------");
+            Console.WriteLine();
+            Console.WriteLine($"Eth: {eth};  usd: {usd}");
+            Console.WriteLine($"Equity: {eth*3500 + usd} $");
 
             Console.WriteLine("End");
             Console.ReadLine();
